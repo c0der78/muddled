ef93def441f256b314b711b6459620eb23fd311f
Ryan Jennings <c0der78@gmail.com>
2012-04-28 19:03:06 -0700

    update doxygen comments

--- src/muddyengine/engine.h
+++ src/muddyengine/engine.h
@@ -1,23 +1,32 @@
 
-/******************************************************************************
- *         __  __           _     _         ____  _       _                   *
- *        |  \/  |_   _  __| | __| |_   _  |  _ \| | __ _(_)_ __  ___         *
- *        | |\/| | | | |/ _` |/ _` | | | | | |_) | |/ _` | | '_ \/ __|        *
- *        | |  | | |_| | (_| | (_| | |_| | |  __/| | (_| | | | | \__ \        *
- *        |_|  |_|\__,_|\__,_|\__,_|\__, | |_|   |_|\__,_|_|_| |_|___/        *
- *                                  |___/                                     *
- *                                                                            *
- *    (C) 2010 by Ryan Jennings <c0der78@gmail.com> www.ryan-jennings.net     *
- *	           Many thanks to creators of muds before me.                 *
- *                                                                            *
- *        In order to use any part of this Mud, you must comply with the      *
- *     license in 'license.txt'.  In particular, you may not remove either    *
- *                        of these copyright notices.                         *
- *                                                                            *
- *       Much time and thought has gone into this software and you are        *
- *     benefitting.  I hope that you share your changes too.  What goes       *
- *                            around, comes around.                           *
- ******************************************************************************/
+/** @mainpage
+ * @htmlonly
+ <pre>
+ ******************************************************************************
+ *         __  __           _     _         ____  _       _                   
+ *        |  \/  |_   _  __| | __| |_   _  |  _ \| | __ _(_)_ __  ___         
+ *        | |\/| | | | |/ _` |/ _` | | | | | |_) | |/ _` | | '_ \/ __|        
+ *        | |  | | |_| | (_| | (_| | |_| | |  __/| | (_| | | | | \__ \        
+ *        |_|  |_|\__,_|\__,_|\__,_|\__, | |_|   |_|\__,_|_|_| |_|___/        
+ *                                  |___/                                     
+ *                                                                            
+ *    (C) 2010 by Ryan Jennings <c0der78@gmail.com> www.ryan-jennings.net     
+ * 	           Many thanks to creators of muds before me.                     
+ *                                                                            
+ *        In order to use any part of this Mud, you must comply with the      
+ *     license in 'license.txt'.  In particular, you may not remove either    
+ *                        of these copyright notices.                         
+ *                                                                            
+ *       Much time and thought has gone into this software and you are        
+ *     benefitting.  I hope that you share your changes too.  What goes       
+ *                            around, comes around.                           
+ ******************************************************************************
+ </pre>
+ * @endhtmlonly
+ * @section todo TODO
+ * 
+ * todo
+ */
 
 #ifndef ENGINE_H
 #define ENGINE_H
